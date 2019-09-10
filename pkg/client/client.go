3318662f1e6652409496daa595559610b9976db7
Lorenzo Fontana <lo@linux.com>
2019-09-10 14:51:13 +0200

    update: godoc for client.go
    Co-Authored-By: Leonardo Di Donato <leodidonato@gmail.com>
    Co-Authored-By: Leonardo Grasso <me@leonardograsso.com>
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -7,18 +7,20 @@ import (
 	"google.golang.org/grpc"
 )
 
-// Client ...
+// Client is a wrapper for the gRPC connection
+// it allows to connect to a Falco gRPC server.
+// It is created using the function NewForConfig(config *Config) .
 type Client struct {
 	conn *grpc.ClientConn
 }
 
-// Config ...
+// Config is the configuration definition for connecting to a Falco gRPC server.
 type Config struct {
 	Target  string
 	Options []grpc.DialOption
 }
 
-// NewForConfig ...
+// NewForConfig is used to create a new Falco gRPC client.
 func NewForConfig(config *Config) (*Client, error) {
 	// Set up a connection to the server.
 	conn, err := grpc.Dial(config.Target, config.Options...)
@@ -31,7 +33,8 @@ func NewForConfig(config *Config) (*Client, error) {
 	}, nil
 }
 
-// Output ...
+// Output is the client for Falco Outputs.
+// When using it you can use `subscribe()` to receive a stream of falco output events.
 func (c *Client) Output() (output.FalcoOutputServiceClient, error) {
 	if err := c.checkConn(); err != nil {
 		return nil, err
@@ -39,7 +42,7 @@ func (c *Client) Output() (output.FalcoOutputServiceClient, error) {
 	return output.NewFalcoOutputServiceClient(c.conn), nil
 }
 
-// Close ...
+// Close the connection to the falco gRPC server.
 func (c *Client) Close() error {
 	if err := c.checkConn(); err != nil {
 		return err
