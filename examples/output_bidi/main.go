034fcbbb22ecbb2676c452fcc7cbde6b5edf2b57
Lorenzo Fontana <lo@linux.com>
2020-06-08 12:01:40 +0200

    update(examples): update the outputs examples to use the new OutputsWatch
    Co-Authored-By: Leonardo Di Donato <leodidonato@gmail.com>
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- examples/output_bidi/main.go
+++ examples/output_bidi/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"log"
 	"time"
 
@@ -12,51 +11,31 @@ import (
 	"github.com/gogo/protobuf/jsonpb"
 )
 
+func printOutput(res *outputs.Response) error {
+	out, err := (&jsonpb.Marshaler{}).MarshalToString(res)
+	if err != nil {
+		return err
+	}
+	fmt.Println(out)
+	return nil
+}
+
 func main() {
-	//Set up a connection to the server.
 	c, err := client.NewForConfig(context.Background(), &client.Config{
-		Hostname: "localhost",
-		Port:     5060,
-		// CertFile:   "/etc/falco/certs/client.crt",
-		// KeyFile:    "/etc/falco/certs/client.key",
-		// CARootFile: "/etc/falco/certs/ca.crt",
-		CertFile:   "/tmp/certs/client.crt",
-		KeyFile:    "/tmp/certs/client.key",
-		CARootFile: "/tmp/certs/ca.crt",
+		Hostname:   "localhost",
+		Port:       5060,
+		CertFile:   "/etc/falco/certs/client.crt",
+		KeyFile:    "/etc/falco/certs/client.key",
+		CARootFile: "/etc/falco/certs/ca.crt",
 	})
 	if err != nil {
 		log.Fatalf("unable to connect: %v", err)
 	}
 	defer c.Close()
-	outputsClient, err := c.Outputs()
-	if err != nil {
-		log.Fatalf("unable to obtain an output client: %v", err)
-	}
-
 	ctx := context.Background()
-	fcs, err := outputsClient.Sub(ctx)
-	if err != nil {
-		log.Fatalf("could not subscribe: %v", err)
-	}
 
-	go func() {
-		for {
-			fcs.Send(&outputs.Request{})
-			time.Sleep(time.Second * 5)
-		}
-	}()
-	for {
-		res, err := fcs.Recv()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf("error closing stream after EOF: %v", err)
-		}
-		out, err := (&jsonpb.Marshaler{}).MarshalToString(res)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(out)
+	err = c.OutputsWatch(ctx, printOutput, time.Second*1)
+	if err != nil {
+		log.Fatalf("outputs watch error: %v", err)
 	}
 }
