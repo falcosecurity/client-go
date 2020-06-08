21da9cd7720feb0fd9dae97db02848d91a5d635a
Lorenzo Fontana <lo@linux.com>
2020-06-08 12:01:40 +0200

    update(examples): update the outputs examples to use the new OutputsWatch
    Co-Authored-By: Leonardo Di Donato <leodidonato@gmail.com>
    Signed-off-by: Lorenzo Fontana <lo@linux.com>

--- examples/output_unix_socket_bidi/main.go
+++ examples/output_unix_socket_bidi/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"io"
 	"log"
 	"time"
 
@@ -11,6 +10,15 @@ import (
 	"github.com/falcosecurity/client-go/pkg/client"
 	"github.com/gogo/protobuf/jsonpb"
 )
+
+func printOutput(res *outputs.Response) error {
+	out, err := (&jsonpb.Marshaler{}).MarshalToString(res)
+	if err != nil {
+		return err
+	}
+	fmt.Println(out)
+	return nil
+}
 
 func main() {
 	//Set up a connection to the server.
@@ -21,35 +29,10 @@ func main() {
 		log.Fatalf("unable to connect: %v", err)
 	}
 	defer c.Close()
-	outputsClient, err := c.Outputs()
-	if err != nil {
-		log.Fatalf("unable to obtain an output client: %v", err)
-	}
-
 	ctx := context.Background()
-	fcs, err := outputsClient.Sub(ctx)
-	if err != nil {
-		log.Fatalf("could not subscribe: %v", err)
-	}
 
-	go func() {
-		for {
-			fcs.Send(&outputs.Request{})
-			time.Sleep(time.Second * 5)
-		}
-	}()
-	for {
-		res, err := fcs.Recv()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf("error closing stream after EOF: %v", err)
-		}
-		out, err := (&jsonpb.Marshaler{}).MarshalToString(res)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(out)
+	err = c.OutputsWatch(ctx, printOutput, time.Second*1)
+	if err != nil {
+		log.Fatalf("outputs watch error: %v", err)
 	}
 }
