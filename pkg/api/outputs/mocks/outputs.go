7dd39597c62dd377e6bfde93b252103bd4439dd4
Roberto Scolaro <roberto.scolaro21@gmail.com>
2023-04-15 11:31:11 +0000

    update(pkg/api): regenerate protobuf stubs from newest definitions
    Signed-off-by: Roberto Scolaro <roberto.scolaro21@gmail.com>

--- pkg/api/outputs/mocks/outputs.go
+++ pkg/api/outputs/mocks/outputs.go
@@ -6,37 +6,38 @@ package mock_outputs
 
 import (
 	context "context"
+	reflect "reflect"
+
 	outputs "github.com/falcosecurity/client-go/pkg/api/outputs"
 	gomock "github.com/golang/mock/gomock"
 	grpc "google.golang.org/grpc"
 	metadata "google.golang.org/grpc/metadata"
-	reflect "reflect"
 )
 
-// MockServiceClient is a mock of ServiceClient interface
+// MockServiceClient is a mock of ServiceClient interface.
 type MockServiceClient struct {
 	ctrl     *gomock.Controller
 	recorder *MockServiceClientMockRecorder
 }
 
-// MockServiceClientMockRecorder is the mock recorder for MockServiceClient
+// MockServiceClientMockRecorder is the mock recorder for MockServiceClient.
 type MockServiceClientMockRecorder struct {
 	mock *MockServiceClient
 }
 
-// NewMockServiceClient creates a new mock instance
+// NewMockServiceClient creates a new mock instance.
 func NewMockServiceClient(ctrl *gomock.Controller) *MockServiceClient {
 	mock := &MockServiceClient{ctrl: ctrl}
 	mock.recorder = &MockServiceClientMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockServiceClient) EXPECT() *MockServiceClientMockRecorder {
 	return m.recorder
 }
 
-// Get mocks base method
+// Get mocks base method.
 func (m *MockServiceClient) Get(arg0 context.Context, arg1 *outputs.Request, arg2 ...grpc.CallOption) (outputs.Service_GetClient, error) {
 	m.ctrl.T.Helper()
 	varargs := []interface{}{arg0, arg1}
@@ -49,14 +50,14 @@ func (m *MockServiceClient) Get(arg0 context.Context, arg1 *outputs.Request, arg
 	return ret0, ret1
 }
 
-// Get indicates an expected call of Get
+// Get indicates an expected call of Get.
 func (mr *MockServiceClientMockRecorder) Get(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{arg0, arg1}, arg2...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Get", reflect.TypeOf((*MockServiceClient)(nil).Get), varargs...)
 }
 
-// Sub mocks base method
+// Sub mocks base method.
 func (m *MockServiceClient) Sub(arg0 context.Context, arg1 ...grpc.CallOption) (outputs.Service_SubClient, error) {
 	m.ctrl.T.Helper()
 	varargs := []interface{}{arg0}
@@ -69,37 +70,37 @@ func (m *MockServiceClient) Sub(arg0 context.Context, arg1 ...grpc.CallOption) (
 	return ret0, ret1
 }
 
-// Sub indicates an expected call of Sub
+// Sub indicates an expected call of Sub.
 func (mr *MockServiceClientMockRecorder) Sub(arg0 interface{}, arg1 ...interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{arg0}, arg1...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Sub", reflect.TypeOf((*MockServiceClient)(nil).Sub), varargs...)
 }
 
-// MockService_GetClient is a mock of Service_GetClient interface
+// MockService_GetClient is a mock of Service_GetClient interface.
 type MockService_GetClient struct {
 	ctrl     *gomock.Controller
 	recorder *MockService_GetClientMockRecorder
 }
 
-// MockService_GetClientMockRecorder is the mock recorder for MockService_GetClient
+// MockService_GetClientMockRecorder is the mock recorder for MockService_GetClient.
 type MockService_GetClientMockRecorder struct {
 	mock *MockService_GetClient
 }
 
-// NewMockService_GetClient creates a new mock instance
+// NewMockService_GetClient creates a new mock instance.
 func NewMockService_GetClient(ctrl *gomock.Controller) *MockService_GetClient {
 	mock := &MockService_GetClient{ctrl: ctrl}
 	mock.recorder = &MockService_GetClientMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockService_GetClient) EXPECT() *MockService_GetClientMockRecorder {
 	return m.recorder
 }
 
-// CloseSend mocks base method
+// CloseSend mocks base method.
 func (m *MockService_GetClient) CloseSend() error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "CloseSend")
@@ -107,13 +108,13 @@ func (m *MockService_GetClient) CloseSend() error {
 	return ret0
 }
 
-// CloseSend indicates an expected call of CloseSend
+// CloseSend indicates an expected call of CloseSend.
 func (mr *MockService_GetClientMockRecorder) CloseSend() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CloseSend", reflect.TypeOf((*MockService_GetClient)(nil).CloseSend))
 }
 
-// Context mocks base method
+// Context mocks base method.
 func (m *MockService_GetClient) Context() context.Context {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Context")
@@ -121,13 +122,13 @@ func (m *MockService_GetClient) Context() context.Context {
 	return ret0
 }
 
-// Context indicates an expected call of Context
+// Context indicates an expected call of Context.
 func (mr *MockService_GetClientMockRecorder) Context() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Context", reflect.TypeOf((*MockService_GetClient)(nil).Context))
 }
 
-// Header mocks base method
+// Header mocks base method.
 func (m *MockService_GetClient) Header() (metadata.MD, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Header")
@@ -136,13 +137,13 @@ func (m *MockService_GetClient) Header() (metadata.MD, error) {
 	return ret0, ret1
 }
 
-// Header indicates an expected call of Header
+// Header indicates an expected call of Header.
 func (mr *MockService_GetClientMockRecorder) Header() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Header", reflect.TypeOf((*MockService_GetClient)(nil).Header))
 }
 
-// Recv mocks base method
+// Recv mocks base method.
 func (m *MockService_GetClient) Recv() (*outputs.Response, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Recv")
@@ -151,13 +152,13 @@ func (m *MockService_GetClient) Recv() (*outputs.Response, error) {
 	return ret0, ret1
 }
 
-// Recv indicates an expected call of Recv
+// Recv indicates an expected call of Recv.
 func (mr *MockService_GetClientMockRecorder) Recv() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Recv", reflect.TypeOf((*MockService_GetClient)(nil).Recv))
 }
 
-// RecvMsg mocks base method
+// RecvMsg mocks base method.
 func (m *MockService_GetClient) RecvMsg(arg0 interface{}) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "RecvMsg", arg0)
@@ -165,13 +166,13 @@ func (m *MockService_GetClient) RecvMsg(arg0 interface{}) error {
 	return ret0
 }
 
-// RecvMsg indicates an expected call of RecvMsg
+// RecvMsg indicates an expected call of RecvMsg.
 func (mr *MockService_GetClientMockRecorder) RecvMsg(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RecvMsg", reflect.TypeOf((*MockService_GetClient)(nil).RecvMsg), arg0)
 }
 
-// SendMsg mocks base method
+// SendMsg mocks base method.
 func (m *MockService_GetClient) SendMsg(arg0 interface{}) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "SendMsg", arg0)
@@ -179,13 +180,13 @@ func (m *MockService_GetClient) SendMsg(arg0 interface{}) error {
 	return ret0
 }
 
-// SendMsg indicates an expected call of SendMsg
+// SendMsg indicates an expected call of SendMsg.
 func (mr *MockService_GetClientMockRecorder) SendMsg(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SendMsg", reflect.TypeOf((*MockService_GetClient)(nil).SendMsg), arg0)
 }
 
-// Trailer mocks base method
+// Trailer mocks base method.
 func (m *MockService_GetClient) Trailer() metadata.MD {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Trailer")
@@ -193,36 +194,36 @@ func (m *MockService_GetClient) Trailer() metadata.MD {
 	return ret0
 }
 
-// Trailer indicates an expected call of Trailer
+// Trailer indicates an expected call of Trailer.
 func (mr *MockService_GetClientMockRecorder) Trailer() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Trailer", reflect.TypeOf((*MockService_GetClient)(nil).Trailer))
 }
 
-// MockService_SubClient is a mock of Service_SubClient interface
+// MockService_SubClient is a mock of Service_SubClient interface.
 type MockService_SubClient struct {
 	ctrl     *gomock.Controller
 	recorder *MockService_SubClientMockRecorder
 }
 
-// MockService_SubClientMockRecorder is the mock recorder for MockService_SubClient
+// MockService_SubClientMockRecorder is the mock recorder for MockService_SubClient.
 type MockService_SubClientMockRecorder struct {
 	mock *MockService_SubClient
 }
 
-// NewMockService_SubClient creates a new mock instance
+// NewMockService_SubClient creates a new mock instance.
 func NewMockService_SubClient(ctrl *gomock.Controller) *MockService_SubClient {
 	mock := &MockService_SubClient{ctrl: ctrl}
 	mock.recorder = &MockService_SubClientMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockService_SubClient) EXPECT() *MockService_SubClientMockRecorder {
 	return m.recorder
 }
 
-// CloseSend mocks base method
+// CloseSend mocks base method.
 func (m *MockService_SubClient) CloseSend() error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "CloseSend")
@@ -230,13 +231,13 @@ func (m *MockService_SubClient) CloseSend() error {
 	return ret0
 }
 
-// CloseSend indicates an expected call of CloseSend
+// CloseSend indicates an expected call of CloseSend.
 func (mr *MockService_SubClientMockRecorder) CloseSend() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CloseSend", reflect.TypeOf((*MockService_SubClient)(nil).CloseSend))
 }
 
-// Context mocks base method
+// Context mocks base method.
 func (m *MockService_SubClient) Context() context.Context {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Context")
@@ -244,13 +245,13 @@ func (m *MockService_SubClient) Context() context.Context {
 	return ret0
 }
 
-// Context indicates an expected call of Context
+// Context indicates an expected call of Context.
 func (mr *MockService_SubClientMockRecorder) Context() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Context", reflect.TypeOf((*MockService_SubClient)(nil).Context))
 }
 
-// Header mocks base method
+// Header mocks base method.
 func (m *MockService_SubClient) Header() (metadata.MD, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Header")
@@ -259,13 +260,13 @@ func (m *MockService_SubClient) Header() (metadata.MD, error) {
 	return ret0, ret1
 }
 
-// Header indicates an expected call of Header
+// Header indicates an expected call of Header.
 func (mr *MockService_SubClientMockRecorder) Header() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Header", reflect.TypeOf((*MockService_SubClient)(nil).Header))
 }
 
-// Recv mocks base method
+// Recv mocks base method.
 func (m *MockService_SubClient) Recv() (*outputs.Response, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Recv")
@@ -274,13 +275,13 @@ func (m *MockService_SubClient) Recv() (*outputs.Response, error) {
 	return ret0, ret1
 }
 
-// Recv indicates an expected call of Recv
+// Recv indicates an expected call of Recv.
 func (mr *MockService_SubClientMockRecorder) Recv() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Recv", reflect.TypeOf((*MockService_SubClient)(nil).Recv))
 }
 
-// RecvMsg mocks base method
+// RecvMsg mocks base method.
 func (m *MockService_SubClient) RecvMsg(arg0 interface{}) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "RecvMsg", arg0)
@@ -288,13 +289,13 @@ func (m *MockService_SubClient) RecvMsg(arg0 interface{}) error {
 	return ret0
 }
 
-// RecvMsg indicates an expected call of RecvMsg
+// RecvMsg indicates an expected call of RecvMsg.
 func (mr *MockService_SubClientMockRecorder) RecvMsg(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RecvMsg", reflect.TypeOf((*MockService_SubClient)(nil).RecvMsg), arg0)
 }
 
-// Send mocks base method
+// Send mocks base method.
 func (m *MockService_SubClient) Send(arg0 *outputs.Request) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Send", arg0)
@@ -302,13 +303,13 @@ func (m *MockService_SubClient) Send(arg0 *outputs.Request) error {
 	return ret0
 }
 
-// Send indicates an expected call of Send
+// Send indicates an expected call of Send.
 func (mr *MockService_SubClientMockRecorder) Send(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Send", reflect.TypeOf((*MockService_SubClient)(nil).Send), arg0)
 }
 
-// SendMsg mocks base method
+// SendMsg mocks base method.
 func (m *MockService_SubClient) SendMsg(arg0 interface{}) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "SendMsg", arg0)
@@ -316,13 +317,13 @@ func (m *MockService_SubClient) SendMsg(arg0 interface{}) error {
 	return ret0
 }
 
-// SendMsg indicates an expected call of SendMsg
+// SendMsg indicates an expected call of SendMsg.
 func (mr *MockService_SubClientMockRecorder) SendMsg(arg0 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SendMsg", reflect.TypeOf((*MockService_SubClient)(nil).SendMsg), arg0)
 }
 
-// Trailer mocks base method
+// Trailer mocks base method.
 func (m *MockService_SubClient) Trailer() metadata.MD {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Trailer")
@@ -330,7 +331,7 @@ func (m *MockService_SubClient) Trailer() metadata.MD {
 	return ret0
 }
 
-// Trailer indicates an expected call of Trailer
+// Trailer indicates an expected call of Trailer.
 func (mr *MockService_SubClientMockRecorder) Trailer() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Trailer", reflect.TypeOf((*MockService_SubClient)(nil).Trailer))
--- pkg/api/version/mocks/version.go
+++ pkg/api/version/mocks/version.go
@@ -6,36 +6,37 @@ package mock_version
 
 import (
 	context "context"
+	reflect "reflect"
+
 	version "github.com/falcosecurity/client-go/pkg/api/version"
 	gomock "github.com/golang/mock/gomock"
 	grpc "google.golang.org/grpc"
-	reflect "reflect"
 )
 
-// MockServiceClient is a mock of ServiceClient interface
+// MockServiceClient is a mock of ServiceClient interface.
 type MockServiceClient struct {
 	ctrl     *gomock.Controller
 	recorder *MockServiceClientMockRecorder
 }
 
-// MockServiceClientMockRecorder is the mock recorder for MockServiceClient
+// MockServiceClientMockRecorder is the mock recorder for MockServiceClient.
 type MockServiceClientMockRecorder struct {
 	mock *MockServiceClient
 }
 
-// NewMockServiceClient creates a new mock instance
+// NewMockServiceClient creates a new mock instance.
 func NewMockServiceClient(ctrl *gomock.Controller) *MockServiceClient {
 	mock := &MockServiceClient{ctrl: ctrl}
 	mock.recorder = &MockServiceClientMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockServiceClient) EXPECT() *MockServiceClientMockRecorder {
 	return m.recorder
 }
 
-// Version mocks base method
+// Version mocks base method.
 func (m *MockServiceClient) Version(arg0 context.Context, arg1 *version.Request, arg2 ...grpc.CallOption) (*version.Response, error) {
 	m.ctrl.T.Helper()
 	varargs := []interface{}{arg0, arg1}
@@ -48,7 +49,7 @@ func (m *MockServiceClient) Version(arg0 context.Context, arg1 *version.Request,
 	return ret0, ret1
 }
 
-// Version indicates an expected call of Version
+// Version indicates an expected call of Version.
 func (mr *MockServiceClientMockRecorder) Version(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{arg0, arg1}, arg2...)
